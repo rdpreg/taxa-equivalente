5e42852a4ce58db63490e0ee4085ddcb79b22d3f
Rafael Dadoorian <30996645+rdpreg@users.noreply.github.com>
2025-04-25 15:35:30 -0300

    Update calculadora_taxa_equivalente.py

--- calculadora_taxa_equivalente.py
+++ calculadora_taxa_equivalente.py
@@ -9,27 +9,13 @@ def calcular_taxa_equivalente(taxa, n1, n2):
 # Configuração da página
 st.set_page_config(page_title="Calculadora de Taxa Equivalente", layout="centered")
 
-# 🔽 Esconde os botões + e -
-st.markdown("""
-    <style>
-    input[type=number]::-webkit-inner-spin-button, 
-    input[type=number]::-webkit-outer-spin-button { 
-        -webkit-appearance: none;
-        margin: 0;
-    }
-    input[type=number] {
-        -moz-appearance: textfield;
-    }
-    </style>
-""", unsafe_allow_html=True)
-
 # Título
 st.title("📈 Cálculo de Taxa Equivalente")
 
 # Área de inputs
 col1, col2 = st.columns(2)
 with col1:
-    taxa = st.number_input("Taxa de juros (%)", min_value=0.0, format="%.4f")
+    taxa = st.number_input("Taxa de juros (%)", min_value=0.0)
 with col2:
     periodo_de = st.number_input("Período (de)", min_value=1, format="%d")
 
